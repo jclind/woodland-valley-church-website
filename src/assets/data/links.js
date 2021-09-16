3c1d56503c408a78b9c1c304c5c14726cf32f8de
Jesse Lind <Jesseclind@gmail.com>
2021-09-16 16:56:52 -0400

    Add link array and add links to navbar with loop

---
+++ src/assets/data/links.js
@@ -0,0 +1,34 @@
+export const links = [
+  {
+    name: 'home',
+    path: '/',
+  },
+  {
+    name: 'about',
+    path: '/about',
+  },
+  {
+    name: 'beliefs',
+    path: '/our-beliefs',
+  },
+  {
+    name: 'events',
+    path: '/events',
+  },
+  {
+    name: 'give',
+    path: '/give',
+  },
+  {
+    name: 'ministries',
+    path: '/ministries',
+  },
+  {
+    name: 'Sermons',
+    path: '/Sermons',
+  },
+  {
+    name: 'Visit',
+    path: '/Visit',
+  },
+]
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Navbar.css'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/images/woodland-valley-logo.png'
+import { links } from '../../assets/data/links'
+
 const Navbar = () => {
+  const [show, setShow] = useState(false)
   return (
     <nav>
       <div className='nav-center'>
@@ -9,6 +13,21 @@ const Navbar = () => {
           <Link to='/'>
             <img src={logo} alt='Woodland Valley Logo' />
           </Link>
+        </div>
+        <div className={show ? 'nav-links show-links' : 'nav-links'}>
+          {links.map((item, idx) => {
+            return (
+              <Link
+                key={idx}
+                to={item.path}
+                className='nav-link'
+                activeClassName='active-link'
+                onClick={() => setShow(false)}
+              >
+                {item.name}
+              </Link>
+            )
+          })}
         </div>
       </div>
     </nav>
