fef4c2225112f8cf0a49fb006d79ce46e41cdfb4
Jesse Lind <Jesseclind@gmail.com>
2021-09-20 11:46:57 -0400

    Add about home page component

--- src/components/AboutThumbnail/AboutThumbnail.js
+++ src/components/AboutThumbnail/AboutThumbnail.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import './AboutThumbnail.css'
+import churchImage from '../../assets/images/wv-church-image.jpg'
 const AboutThumbnail = () => {
   return (
-    <div className='about-container'>
-      <h1>About Thumbnail</h1>
+    <div className='about-component-container'>
+      <div className='content'>
+        <h1>About WVC</h1>
+        <p>
+          The purpose of Woodland Valley Church is to celebrate our love for
+          God, care for the needs of His children, carry His Message to the
+          world, come together as His family and commit to helping that family
+          grow closer to Him. This purpose is drawn from God’s Holy Word.
+        </p>
+        <button className='btn'>More About Us</button>
+      </div>
+      <div className='image'>
+        <img src={churchImage} alt='' className='img' />
+      </div>
     </div>
   )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import AboutThumbnail from '../../components/AboutThumbnail/AboutThumbnail'
-
+import './Home.css'
 const Home = () => {
   return (
     <div className='page'>
       <Hero />
       <AboutThumbnail />
+      {/* <div className='about-transition'></div> */}
+      <br />
+      <br />
+      <br />
     </div>
   )
 }
