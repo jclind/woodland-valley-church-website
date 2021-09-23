15bddfd19fc98456fc4021795713e3514fc78fdc
Jesse Lind <Jesseclind@gmail.com>
2021-09-22 20:18:46 -0400

    Add Footer Component (Home Page)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,51 @@
 import React from 'react'
+import { AiOutlineInstagram, AiFillMail, AiFillPhone } from 'react-icons/ai'
+import { FaYoutube } from 'react-icons/fa'
+import { GrFacebookOption } from 'react-icons/gr'
 import './Footer.css'
-import logo from '../../assets/images/woodland-valley-logo.png'
+
 const Footer = () => {
   return (
     <footer className='section-container'>
       <div className='section'>
-        <img src={logo} className='logo' alt='WVC logo' />
-        <h1 className='section-title'>This is the footer</h1>
+        <h1 className='title'>Woodland Valley Church</h1>
+        <h3 className='subtitle'>Together we make a difference.</h3>
+        <div className='social-links'>
+          <div className='icon'>
+            <AiOutlineInstagram />
+          </div>
+          <div className='icon'>
+            <GrFacebookOption />
+          </div>
+          <div className='icon'>
+            <FaYoutube />
+          </div>
+        </div>
+        <div className='contact-info'>
+          <div className='email'>
+            <AiFillMail className='icon' />
+            wvcaa@zoominternet.net
+          </div>
+          <div className='phone'>
+            <AiFillPhone className='icon' />
+            724-625-4500
+          </div>
+          <div className='address'>
+            <span Crowe Avenuespan className='avoid-wrap'>
+              225 Crowe Avenue
+            </span>
+            {', '}
+            <span Crowe Avenuespan className='avoid-wrap'>
+              P.O. Box 815
+            </span>{' '}
+            <span Crowe Avenuespan className='avoid-wrap'>
+              Mars, PA 16046
+            </span>
+          </div>
+        </div>
+        <div className='copyright'>
+          &copy;{new Date().getFullYear} Woodland Valley Church
+        </div>
       </div>
     </footer>
   )
