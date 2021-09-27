5c025b55921013e10908949314e7a6c53322fc89
Jesse Lind <Jesseclind@gmail.com>
2021-09-27 16:55:20 -0400

    Add other ways to give section (give page)

--- src/pages/Give/Give.js
+++ src/pages/Give/Give.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Give.css'
+import { BiMailSend, BiBox } from 'react-icons/bi'
 
 const Give = () => {
   return (
@@ -22,6 +23,39 @@ const Give = () => {
             effort, please notate as such with your donation. We incur an
             administrative fee of 2.2% plus a 30 cent transaction fee.
           </p>
+        </div>
+      </section>
+      <section className='other-options-container section-container'>
+        <div className='section other-options'>
+          <h1 className='section-title'>Other Ways To Give</h1>
+          <div className='articles'>
+            <article className='in-person'>
+              <h3 className='section-secondary-title title'>
+                <BiBox className='icon' />
+                In Person
+              </h3>
+              <p className='section-text'>
+                Join us for Sunday Service and drop your donation in the
+                offering or feel free to place in our lock box outside the main
+                entrance.
+              </p>
+            </article>
+            <article className='mail'>
+              <h3 className='section-secondary-title title'>
+                <BiMailSend className='icon' />
+                By Mail
+              </h3>
+              <p className='section-text'>
+                Simply mail your donation to:
+                <br />{' '}
+              </p>
+              <p className='strong section-text'>
+                Woodland Valley Church
+                <br /> P.O. Box 815
+                <br /> Mars, PA 16046
+              </p>
+            </article>
+          </div>
         </div>
       </section>
     </div>
