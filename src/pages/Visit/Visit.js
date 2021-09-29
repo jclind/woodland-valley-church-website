2b068933f8abd70aa2030a538bd5b260d05ef796
Jesse Lind <Jesseclind@gmail.com>
2021-09-29 13:52:36 -0400

    Begin Visit Page, Working on hero css and button functionality

--- src/pages/Visit/Visit.js
+++ src/pages/Visit/Visit.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import './Visit.css'
 
 const Visit = () => {
   return (
-    <div>
-      <h1>Visit</h1>
+    <div className='page visit-page'>
+      <div className='page-hero'>
+        <h1 className='page-hero-title'>Visit Us</h1>
+        <button className='section-button'>Directions</button>
+      </div>
     </div>
   )
 }
