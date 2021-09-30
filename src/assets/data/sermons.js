cda844619e2849adc172616330598572d937c2cb
Jesse Lind <Jesseclind@gmail.com>
2021-09-30 16:11:10 -0400

    add sermon data with links and add parse function to get video id from link, add to iframe embeder in html

---
+++ src/assets/data/sermons.js
@@ -0,0 +1,49 @@
+export const sermons = [
+  {
+    link: 'https://youtu.be/ACOwdrlbatU',
+    title: 'Opportunity for Impact',
+    description:
+      'Paul saw a great door opening for him, he saw that it was a door of opportunity, a door of obligation and a door of opposition.  1 Corinthians 16:5-11',
+    date: 'Sun Sep 26 2021 10:00:00 GMT-0400 (Eastern Daylight Time)',
+  },
+  {
+    link: 'https://youtu.be/l63_GNGlwTM',
+    title: 'Opportunity for Generosity',
+    description:
+      'In 1 Corinthians 16, Paul finishes with a number of opportunities.  We look at the opportunity of generosity in verses 1-4.',
+    date: 'Sun Sep 19 2021 10:00:00 GMT-0400 (Eastern Daylight Time)',
+  },
+  {
+    link: 'https://youtu.be/rHy1F3LQVXk',
+    title: 'Victory over Tragedy',
+    description: 'How do we endure hardships?  Psalm 91 gives us answers.',
+    date: 'Sun Sep 12 2021 10:00:00 GMT-0400 (Eastern Daylight Time)',
+  },
+  {
+    link: 'https://youtu.be/flcTdpqsE3U',
+    title: 'The Good News',
+    description: 'Dave Otto shares "The Good News" drawing upon Romans 5:1-6',
+    date: 'Sun Sep 05 2021 10:00:00 GMT-0400 (Eastern Daylight Time)',
+  },
+]
+
+export const sermonSeries = [
+  {
+    name: 'Have You Ever Asked Why?',
+    img: 'https://i.ibb.co/DVgpBkB/why-series-JPG.jpg',
+    description: 'Dave Otto shares "The Good News" drawing upon Romans 5:1-6',
+    link: 'https://www.youtube.com/playlist?list=PLDvHdDGRDXJZjS6TWJr15a0sN7R5xkNc5',
+  },
+  {
+    name: 'Living Life Through The Gospel (1 Corinithians)',
+    img: 'https://i.ibb.co/xgzrxqV/1-Corinthian-series.jpg',
+    description: '',
+    link: 'https://www.youtube.com/playlist?list=PLDvHdDGRDXJatrm9TKY0QOcTl3YYYwzzW',
+  },
+  {
+    name: 'Simply Jesus',
+    img: 'https://i.ibb.co/3BfkFn8/Simply-Jesus-Series-Picture.jpg',
+    description: '',
+    link: 'https://www.youtube.com/playlist?list=PLDvHdDGRDXJZxGeU1Sp0ZY94MJFzzuTWl',
+  },
+]
--- src/pages/Sermons/Sermons.js
+++ src/pages/Sermons/Sermons.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
-import $ from 'jquery'
+import { sermons, sermonSeries } from '../../assets/data/sermons'
+import { youtubeParser } from '../../util/youtubeParser'
+// import $ from 'jquery'
 
 const Sermons = () => {
-  // const [ids, setIds] = useState([])
+  const sortedSermons = [...sermons].sort(function (a, b) {
+    return new Date(b.date) - new Date(a.date)
+  })
+  // const [sermons, setSermons] = useState([])
   // const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCc6IeASF19Yk7nM3BbOyQng&key=AIzaSyALaz3TYItdDed52907J0A-KlgoipMxIFg`
   // fetch(url)
   //   .then(res => res.json())
@@ -16,51 +21,25 @@ const Sermons = () => {
   //     // )
   //   })
   //   .catch(err => console.log(err))
-  const channelID = 'UCc6IeASF19Yk7nM3BbOyQng'
-  const reqURL = 'https://www.youtube.com/feeds/videos.xml?channel_id='
-  $.getJSON(
-    'https://api.rss2json.com/v1/api.json?rss_url=' +
-      encodeURIComponent(reqURL) +
-      channelID,
-    function (data) {
-      var link = data.items[0].link
-      console.log(data.items)
-      var id = link.substr(link.indexOf('=') + 1)
-      console.log(id)
-      $('#youtube_video1').attr(
-        'src',
-        'https://youtube.com/embed/' + id + '?controls=0&showinfo=0&rel=0'
-      )
-    }
-  )
-  $.getJSON(
-    'https://api.rss2json.com/v1/api.json?rss_url=' +
-      encodeURIComponent(reqURL) +
-      channelID,
-    function (data) {
-      var link = data.items[1].link
-      var id = link.substr(link.indexOf('=') + 1)
-      console.log(id)
-      $('#youtube_video2').attr(
-        'src',
-        'https://youtube.com/embed/' + id + '?controls=0&showinfo=0&rel=0'
-      )
-    }
-  )
+  //
+
   return (
     <div>
-      <iframe
-        id='youtube_video1'
-        width='600'
-        height='340'
-        frameborder='0'
-      ></iframe>
-      <iframe
-        id='youtube_video2'
-        width='600'
-        height='340'
-        frameborder='0'
-      ></iframe>
+      {sortedSermons.map((sermon, idx) => {
+        const currID = youtubeParser(sermon.link)
+        const src = `https://www.youtube.com/embed/${currID}`
+        return (
+          <iframe
+            width='1280'
+            height='720'
+            src={src}
+            title='YouTube video player'
+            frameborder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowfullscreen
+          ></iframe>
+        )
+      })}
     </div>
   )
 }
